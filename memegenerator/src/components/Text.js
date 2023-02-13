f3ec30575f8f2edbf17ec4b593813036cb5d5796
Blancher <108644584+Blancher@users.noreply.github.com>
2023-02-12 21:38:57 -0800

    Create Text.js

---
+++ memegenerator/src/components/Text.js
@@ -0,0 +1,37 @@
+import {useState} from 'react';
+
+export default function Text(props) {
+    const [display, setDisplay] = useState(false);
+    const changeVisibility = () => {
+        setDisplay(prev => !prev);
+    }
+    const down = (e) => {
+        props.down(props.position);
+        e.target.parentElement.parentElement.style.top = `${props.yOffset}px`;
+    };
+    const up = (e) => {
+        props.up(props.position);
+        e.target.parentElement.parentElement.style.top = `${props.yOffset}px`;
+    };
+    const right = (e) => {
+        props.right(props.position);
+        e.target.parentElement.parentElement.style.left = `${props.xOffset}px`;
+    };
+    const left = (e) => {
+        props.left(props.position);
+        e.target.parentElement.parentElement.style.left = `${props.xOffset}px`;
+    };
+    return (
+        <div className='relative' style={{top: props.position === 'top' ? '220px' : '525px', width: '180px'}}>
+            <p onClick={changeVisibility}>{props.text}</p>
+            {display && (
+                <div className='flex'>
+                    <button onClick={up} className='direction'>↑</button>
+                    <button onClick={down} className='direction'>↓</button>
+                    <button onClick={left} className='direction'>←</button>
+                    <button onClick={right} className='direction'>→</button>
+                </div>
+            )}
+        </div>
+    );
+}
